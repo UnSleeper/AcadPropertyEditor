b6b4eb8373b9871855788b41f4df101acff88c85
IvanVinnikov <Vinniwata@yandex.ru>
2021-03-29 13:11:47 +0700

    Add project files.

---
+++ AcadPropertyEditor/EditWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.Data.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace AcadPropertyEditor
+{
+    /// <summary>
+    /// Interaction logic for EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        private LayersData LayersListData;
+        public EditWindow()
+        {
+            InitializeComponent();
+            LayersListData = new LayersData();
+            DataContext = LayersListData;
+            LayersListData.LayersList.Add(new DataLayer
+            {
+                Name = "Test Name 1"
+            });
+            LayersListData.LayersList.Add(new DataLayer
+            {
+                Name = "Test Name 2"
+            });
+        }
+
+        public class LayersData
+        {
+            public ObservableCollection<DataLayer> LayersList { get; set; } = new ObservableCollection<DataLayer>();
+        }
+
+        public class DataLayer : INotifyPropertyChanged
+        {
+            private string name;
+            public string Name
+            {
+                get => name;
+                set
+                {
+                    name = value;
+                    OnPropertyChanged();
+                }
+            }
+            public ObservableCollection<DataPoint> Points { get; set; }
+            public ObservableCollection<DataLine> Lines { get; set; }
+            public ObservableCollection<DataCircle> Circles { get; set; }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public class DataPoint
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int z { get; set; }
+        }
+        public class DataLine
+        {
+            public int x1 { get; set; }
+            public int y1 { get; set; }
+            public int z1 { get; set; }
+            public int x2 { get; set; }
+            public int y2 { get; set; }
+            public int z2 { get; set; }
+        }
+        public class DataCircle
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int z { get; set; }
+            public int r { get; set; }
+        }
+    }
+}
---
+++ AcadPropertyEditor/Plugin.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Colors;
+using acadApp = Autodesk.AutoCAD.ApplicationServices.Application;
+using WinForm = System.Windows;
+using static AcadPropertyEditor.EditWindow;
+
+namespace AcadPropertyEditor
+{
+    public class Plugin : IExtensionApplication
+    {
+
+        [CommandMethod("StartEdit")]
+        public static void DisplayLayerNames()
+        {
+            var editor = acadApp.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Запускаю окно изменений.." + Environment.NewLine);
+            EditWindow mainView = new EditWindow();
+            if(Application.ShowModalWindow(mainView) != true) return;
+            LayersData layersListData = new LayersData();
+            layersListData = (LayersData)mainView.DataContext;
+            // Get the current document and database
+            Document acDoc = acadApp.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+            using (DocumentLock acLckDoc = acDoc.LockDocument())
+            {
+                // Start a transaction
+                using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+                {
+                    // Open the Layer table for read
+                    LayerTable acLyrTbl;
+
+                    acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+                                                 OpenMode.ForRead) as LayerTable;
+
+                    foreach (ObjectId acObjId in acLyrTbl)
+                    {
+                        LayerTableRecord acLyrTblRec;
+                        acLyrTblRec = acTrans.GetObject(acObjId,
+                                                        OpenMode.ForRead) as LayerTableRecord;
+                        layersListData.LayersList.Add(new DataLayer
+                        {
+                            Name = acLyrTblRec.Name
+                        });
+                    }
+                    // Dispose of the transaction
+                }
+            }
+            mainView.DataContext = layersListData;
+        }
+
+        public void Initialize()
+        {
+            var editor = acadApp.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("Инициализация плагина.." + Environment.NewLine);
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+
+
+    }
+}
